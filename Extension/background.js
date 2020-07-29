af9682586bc924c24dbf3865d4090094d979d1ec
syeda reeha quasar <syedareehaquasar@gmail.com>
2020-07-29 14:46:23 +0000

    Update background.js

--- Extension/background.js
+++ Extension/background.js
@@ -13,3 +13,11 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         }]);
     });
 });
+chrome.extension.onRequest.addListener(function(prediction){
+    if (prediction == 1){
+        alert("Warning: Phishing detected!!");
+    }
+    else if (prediction == -1){
+        alert("No phishing detected");
+    }
+});
