1c9fadf69d6d061c0d028adfdc32d056cf6549d5
syeda reeha quasar <syedareehaquasar@gmail.com>
2020-08-25 21:31:33 +0000

    app main file

---
+++ Flask Application/app.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('strength.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = request.form.values()
+    
+    x_test = featureextraction.
+    prediction = model.predict(x_test)
+    print(prediction)
+    output=prediction[0][0]
+    return render_template('index.html', prediction_text='Compressive Strength of Concrete kg/m^3 {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.y_predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
